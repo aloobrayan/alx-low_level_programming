c2b54c3a76e91ba57b7dca5f1ddd6bf90f8b3fec
aloobrayan <aloobrayan@gmail.com>
2022-09-08 14:34:57 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd>
+
+/**
+ * main - print to the user
+ *
+ * Return: 0 for sucess
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
