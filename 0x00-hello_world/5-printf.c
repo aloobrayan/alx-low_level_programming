2834f50c5c26ff7bc6aea669a743a326fcb2ef55
aloobrayan <aloobrayan@gmail.com>
2022-09-08 13:17:38 -0700

    C program that uses printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the output to the user
+ *
+ * Return: 0 for proper exit, and non-zero on otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+	return (0);
+}
