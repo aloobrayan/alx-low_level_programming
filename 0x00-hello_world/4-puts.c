dcdcf6526a77babe42146f349197bf92dad8817c
aloobrayan <aloobrayan@gmail.com>
2022-09-08 12:59:56 -0700

    printing C program to the screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * main - print the output 
+ * Return: print 0 is code runs corectly and non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
