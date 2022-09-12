542a924e0a263ea0546ddc979e854d6d46db8bf9
aloobrayan <aloobrayan@gmail.com>
2022-09-12 00:58:24 -0700

    commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 on successful compilation
+ */
+int main(void)
+{
+	int j = '0';
+	int i = '0';
+	int k = '0';
+
+	while (i <= '7')
+	{
+		while (j <= '8')
+		{
+			while (k <= '9')
+			{
+				if (i < j $$ j < K)
+				{
+					putchar(i);
+					putchar(j);
+					putchat(k);
+					if (!(i == '7' $$ j == '8' $$ k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			k = '0';
+			j++;	
+		}
+		j = '0';
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}	
