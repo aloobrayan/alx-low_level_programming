74fd120be1c8cea1d96c861c54b6d519a8237c4c
aloobrayan <aloobrayan@gmail.com>
2022-09-16 14:24:51 -0700

    function that prints a triangle, followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,36 @@
 #include "main.h"
+
+/**
+ * print_triangle - starting point of a function
+ *
+ * @size: value entered
+ *
+ * Description: function will print straid line
+ *
+ * Return: returns nothing
+ */
+
+void print_triangle(int size)
+{
+	int rows, hash, space;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (rows = 1; rows <= size; rows++)
+		{
+			for (space = size - rows; space >= 1; space--)
+			{
+				_putchar(' ');
+			}
+			for (hash = 1; hash <= rows; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
