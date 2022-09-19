3022420cc8448672d6f3a688db8fc007060b8be4
aloobrayan <aloobrayan@gmail.com>
2022-09-19 20:44:59 +0000

    array and pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - makes the pointer as int and update the value to 98
+ *
+ * @n: pointer to an int
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is array and pointer directory
